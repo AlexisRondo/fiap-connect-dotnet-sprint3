e685641a47b30128c617fd897516e8775f41c182
AlexisRondo <alan101rqr@gmail.com>
2026-05-20 13:58:57 -0300

    criar testes unitarios de NotificacaoService e HistoricoBuscaService

---
+++ tests/FiapConnect.UnitTests/Application/Services/HistoricoBuscaServiceTests.cs
@@ -0,0 +1,118 @@
+using FiapConnect.Application.DTOs.HistoricoBusca;
+using FiapConnect.Application.Interfaces;
+using FiapConnect.Application.Services;
+using FiapConnect.Domain.Entities;
+using FiapConnect.Domain.Exceptions;
+using FiapConnect.Domain.Interfaces;
+using Moq;
+
+namespace FiapConnect.UnitTests.Application.Services;
+
+public class HistoricoBuscaServiceTests
+{
+    [Fact]
+    public async Task RegistrarAsync_QuandoAlunoNaoExisteNoOracle_LancaRecursoNaoEncontradoException()
+    {
+        // Arrange
+        var repositoryMock = new Mock<IHistoricoBuscaRepository>();
+        var oracleMock = new Mock<IOracleClient>();
+        oracleMock
+            .Setup(o => o.ObterUsuarioPorRmAsync("999999"))
+            .ReturnsAsync((Usuario?)null);
+
+        var service = new HistoricoBuscaService(repositoryMock.Object, oracleMock.Object);
+        var request = new RegistrarBuscaRequest
+        {
+            RmAluno = "999999",
+            FiltroDisciplina = "DOTNET",
+            EdicaoChallenge = "ORACLE_2024_2"
+        };
+
+        // Act
+        Func<Task> acao = () => service.RegistrarAsync(request);
+
+        // Assert
+        await Assert.ThrowsAsync<RecursoNaoEncontradoException>(acao);
+    }
+
+    [Fact]
+    public async Task RegistrarAsync_QuandoAlunoExiste_GravaComNomeAlunoDoOracle()
+    {
+        // Arrange
+        var repositoryMock = new Mock<IHistoricoBuscaRepository>();
+        var oracleMock = new Mock<IOracleClient>();
+        oracleMock
+            .Setup(o => o.ObterUsuarioPorRmAsync("560384"))
+            .ReturnsAsync(new Usuario { Rm = "560384", NomeCompleto = "Alexis Rondo", EmailInstitucional = "rm560384@fiap.com.br" });
+        repositoryMock
+            .Setup(r => r.RegistrarAsync(It.IsAny<HistoricoBusca>()))
+            .ReturnsAsync((HistoricoBusca h) => h);
+
+        var service = new HistoricoBuscaService(repositoryMock.Object, oracleMock.Object);
+        var request = new RegistrarBuscaRequest
+        {
+            RmAluno = "560384",
+            FiltroDisciplina = "DOTNET",
+            EdicaoChallenge = "ORACLE_2024_2"
+        };
+
+        // Act
+        await service.RegistrarAsync(request);
+
+        // Assert
+        repositoryMock.Verify(r => r.RegistrarAsync(It.Is<HistoricoBusca>(h => h.NomeAluno == "Alexis Rondo")), Times.Once);
+    }
+
+    [Fact]
+    public async Task RegistrarAsync_QuandoChamado_CalculaTotalGruposRetornadosDoTamanhoDaLista()
+    {
+        // Arrange
+        var repositoryMock = new Mock<IHistoricoBuscaRepository>();
+        var oracleMock = new Mock<IOracleClient>();
+        oracleMock
+            .Setup(o => o.ObterUsuarioPorRmAsync("560384"))
+            .ReturnsAsync(new Usuario { Rm = "560384", NomeCompleto = "Alexis", EmailInstitucional = "rm560384@fiap.com.br" });
+        repositoryMock
+            .Setup(r => r.RegistrarAsync(It.IsAny<HistoricoBusca>()))
+            .ReturnsAsync((HistoricoBusca h) => h);
+
+        var service = new HistoricoBuscaService(repositoryMock.Object, oracleMock.Object);
+        var request = new RegistrarBuscaRequest
+        {
+            RmAluno = "560384",
+            FiltroDisciplina = "DOTNET",
+            EdicaoChallenge = "ORACLE_2024_2",
+            GruposRetornados = new List<GrupoRetornadoDto>
+            {
+                new() { IdGrupo = 1, NomeGrupo = "G1", Percentual = 100, ClassificacaoIa = "ALTA" },
+                new() { IdGrupo = 2, NomeGrupo = "G2", Percentual = 75, ClassificacaoIa = "MEDIA" },
+                new() { IdGrupo = 3, NomeGrupo = "G3", Percentual = 50, ClassificacaoIa = "BAIXA" }
+            }
+        };
+
+        // Act
+        await service.RegistrarAsync(request);
+
+        // Assert
+        repositoryMock.Verify(r => r.RegistrarAsync(It.Is<HistoricoBusca>(h => h.TotalGruposRetornados == 3)), Times.Once);
+    }
+
+    [Fact]
+    public async Task RemoverAsync_QuandoIdNaoExiste_LancaRecursoNaoEncontradoException()
+    {
+        // Arrange
+        var repositoryMock = new Mock<IHistoricoBuscaRepository>();
+        var oracleMock = new Mock<IOracleClient>();
+        repositoryMock
+            .Setup(r => r.ObterPorIdAsync("inexistente"))
+            .ReturnsAsync((HistoricoBusca?)null);
+
+        var service = new HistoricoBuscaService(repositoryMock.Object, oracleMock.Object);
+
+        // Act
+        Func<Task> acao = () => service.RemoverAsync("inexistente");
+
+        // Assert
+        await Assert.ThrowsAsync<RecursoNaoEncontradoException>(acao);
+    }
+}
---
+++ tests/FiapConnect.UnitTests/Application/Services/NotificacaoServiceTests.cs
@@ -0,0 +1,118 @@
+using FiapConnect.Application.DTOs.Notificacao;
+using FiapConnect.Application.Interfaces;
+using FiapConnect.Application.Services;
+using FiapConnect.Domain.Entities;
+using FiapConnect.Domain.Exceptions;
+using FiapConnect.Domain.Interfaces;
+using Moq;
+
+namespace FiapConnect.UnitTests.Application.Services;
+
+public class NotificacaoServiceTests
+{
+    [Fact]
+    public async Task CriarAsync_QuandoDestinatarioNaoExisteNoOracle_LancaRecursoNaoEncontradoException()
+    {
+        // Arrange
+        var repositoryMock = new Mock<INotificacaoRepository>();
+        var oracleMock = new Mock<IOracleClient>();
+        oracleMock
+            .Setup(o => o.ObterUsuarioPorRmAsync("999999"))
+            .ReturnsAsync((Usuario?)null);
+
+        var service = new NotificacaoService(repositoryMock.Object, oracleMock.Object);
+        var request = new CriarNotificacaoRequest
+        {
+            RmDestinatario = "999999",
+            Tipo = "INFO",
+            Titulo = "Teste",
+            Mensagem = "Mensagem teste"
+        };
+
+        // Act
+        Func<Task> acao = () => service.CriarAsync(request);
+
+        // Assert
+        await Assert.ThrowsAsync<RecursoNaoEncontradoException>(acao);
+    }
+
+    [Fact]
+    public async Task CriarAsync_QuandoDadosValidos_GravaNotificacaoComOrigemDotnet()
+    {
+        // Arrange
+        var repositoryMock = new Mock<INotificacaoRepository>();
+        var oracleMock = new Mock<IOracleClient>();
+        oracleMock
+            .Setup(o => o.ObterUsuarioPorRmAsync("560384"))
+            .ReturnsAsync(new Usuario { Rm = "560384", NomeCompleto = "Alexis", EmailInstitucional = "rm560384@fiap.com.br" });
+        repositoryMock
+            .Setup(r => r.CriarAsync(It.IsAny<Notificacao>()))
+            .ReturnsAsync((Notificacao n) => n);
+
+        var service = new NotificacaoService(repositoryMock.Object, oracleMock.Object);
+        var request = new CriarNotificacaoRequest
+        {
+            RmDestinatario = "560384",
+            Tipo = "INFO",
+            Titulo = "Teste",
+            Mensagem = "Mensagem teste",
+            Prioridade = "NORMAL"
+        };
+
+        // Act
+        var resultado = await service.CriarAsync(request);
+
+        // Assert
+        Assert.Equal("DOTNET", resultado.Origem);
+        repositoryMock.Verify(r => r.CriarAsync(It.Is<Notificacao>(n => n.Origem == "DOTNET")), Times.Once);
+    }
+
+    [Fact]
+    public async Task CriarAsync_QuandoPrioridadeVazia_DefineComoNormal()
+    {
+        // Arrange
+        var repositoryMock = new Mock<INotificacaoRepository>();
+        var oracleMock = new Mock<IOracleClient>();
+        oracleMock
+            .Setup(o => o.ObterUsuarioPorRmAsync("560384"))
+            .ReturnsAsync(new Usuario { Rm = "560384", NomeCompleto = "Alexis", EmailInstitucional = "rm560384@fiap.com.br" });
+        repositoryMock
+            .Setup(r => r.CriarAsync(It.IsAny<Notificacao>()))
+            .ReturnsAsync((Notificacao n) => n);
+
+        var service = new NotificacaoService(repositoryMock.Object, oracleMock.Object);
+        var request = new CriarNotificacaoRequest
+        {
+            RmDestinatario = "560384",
+            Tipo = "INFO",
+            Titulo = "Teste",
+            Mensagem = "Mensagem teste",
+            Prioridade = string.Empty
+        };
+
+        // Act
+        await service.CriarAsync(request);
+
+        // Assert
+        repositoryMock.Verify(r => r.CriarAsync(It.Is<Notificacao>(n => n.Prioridade == "NORMAL")), Times.Once);
+    }
+
+    [Fact]
+    public async Task MarcarComoLidaAsync_QuandoIdNaoExiste_LancaRecursoNaoEncontradoException()
+    {
+        // Arrange
+        var repositoryMock = new Mock<INotificacaoRepository>();
+        var oracleMock = new Mock<IOracleClient>();
+        repositoryMock
+            .Setup(r => r.ObterPorIdAsync("inexistente"))
+            .ReturnsAsync((Notificacao?)null);
+
+        var service = new NotificacaoService(repositoryMock.Object, oracleMock.Object);
+
+        // Act
+        Func<Task> acao = () => service.MarcarComoLidaAsync("inexistente");
+
+        // Assert
+        await Assert.ThrowsAsync<RecursoNaoEncontradoException>(acao);
+    }
+}
